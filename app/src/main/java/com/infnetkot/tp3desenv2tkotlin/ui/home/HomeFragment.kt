743d923fc2e97d25eadc559a688f0d3523951a48
Lucas Martins <lucas.martins@ed.company>
2020-06-08 21:32:59 -0300

    Adicionado botão de logout

--- app/src/main/java/com/infnetkot/tp3desenv2tkotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/infnetkot/tp3desenv2tkotlin/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.infnetkot.tp3desenv2tkotlin.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
+import com.infnetkot.tp3desenv2tkotlin.MainActivity
 import com.infnetkot.tp3desenv2tkotlin.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -21,5 +25,17 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_logout.setOnClickListener {
+            var auth = FirebaseAuth.getInstance()
+
+            auth.signOut()
+            var intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
